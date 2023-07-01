66d265bc3d1777b7ff83aba63892393363ec1426
Dias <dias@Diass-MacBook-Air.local>
2023-07-01 15:21:05 -0300

    expose libev symbol to allow loading from a custom location.

--- src/lev.lisp
+++ src/lev.lisp
@@ -1,18 +1,26 @@
 (in-package :cl-user)
 (defpackage lev
   (:use :cl
-        :lev.wrapper))
+        :lev.wrapper)
+  (:export
+   #:libev))
 (in-package :lev)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (pushnew :lev-ev-compat3 *features*)
   (pushnew :lev-ev-full *features*))
 
-(eval-when (:load-toplevel)
-  (cffi:define-foreign-library libev
-    (:darwin (:or "libev.4.dylib" "libev.dylib"))
-    (:unix (:or "libev.4.so" "libev.so.4" "libev.so"))
-    (t (:default "libev")))
+;;; to load libev from a custom location...
 
+;; (ql:quickload :cffi)
+;; (cffi:define-foreign-library lev::libev
+;;   (t "path/to/libev.4.so"))
+;; (cffi:use-foreign-library lev::libev)
+(eval-when (:load-toplevel)
   (unless (cffi:foreign-library-loaded-p 'libev)
+    (cffi:define-foreign-library libev
+      (:darwin (:or "libev.4.dylib" "libev.dylib"))
+      (:unix (:or "libev.4.so" "libev.so.4" "libev.so"))
+      (t (:default "libev")))
+
     (cffi:use-foreign-library libev)))
